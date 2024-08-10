55f5d7d6c42e085b8204999604092d31eefd5bdd
Aben <126703861+abengl@users.noreply.github.com>
2024-08-10 16:22:28 -0500

    learn: data types #4

--- Main.java
+++ Main.java
@@ -8,6 +8,7 @@
  */
 public class Main {
     public static void main(String[] args) {
+        /*
         Person person = new Person();  // object from the class Person
         person.setName("Ana");
         System.out.format("OUTPUT -- The person name is %s.%n", person.getName());
@@ -45,5 +46,45 @@ public class Main {
 
         myRectangle.displayInfo();
         //System.out.println("Area of the rectangle: " + myRectangle.calculateArea());
+        */
+
+        String name = "Alex";
+        int age = 18;
+        System.out.println("Hello, " + name + "! Your age is " + age);
+
+        int max = Integer.MAX_VALUE;
+        int min = Integer.MIN_VALUE;
+        System.out.println("The maximum value for an int = " + max);
+        System.out.println("The minimum value for an int = " + min);
+
+        byte maxByte = Byte.MAX_VALUE;
+        byte minByte = Byte.MIN_VALUE;
+        System.out.println("The maximum value for an byte = " + maxByte);
+        System.out.println("The minimum value for an byte = " + minByte);
+
+        short maxShort = Short.MAX_VALUE;
+        short minShort = Short.MIN_VALUE;
+        System.out.println("The maximum value for an short = " + maxShort);
+        System.out.println("The minimum value for an short = " + minShort);
+
+        long maxLong = Long.MAX_VALUE;
+        long minLong = Long.MIN_VALUE;
+        System.out.println("The maximum value for an long = " + maxLong);
+        System.out.println("The minimum value for an long = " + minLong);
+
+        float maxFloat = Float.MAX_VALUE;
+        float minFloat = Float.MIN_VALUE;
+        System.out.println("The maximum value for an float = " + maxFloat);
+        System.out.println("The minimum value for an float = " + minFloat);
+
+        double maxDouble = Double.MAX_VALUE;
+        double minDouble = Double.MIN_VALUE;
+        System.out.println("The maximum value for a double = " + maxDouble);
+        System.out.println("The minimum value for a double = " + minDouble);
+
+        char letter = 'A';
+        boolean isActive = true;
+        System.out.println("A char = " + letter);
+        System.out.println("A boolean = " + isActive);
     }
 }
