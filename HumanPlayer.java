7c5170123cf519299b9184cb63876179174f947c
Aben <126703861+abengl@users.noreply.github.com>
2024-08-11 13:27:13 -0500

    fix: closing scanner on main class #7

--- HumanPlayer.java
+++ HumanPlayer.java
@@ -4,26 +4,22 @@ import java.util.Scanner;
  * Represents the human player
  */
 public class HumanPlayer extends Player {
-    Scanner input = new Scanner(System.in);
+    Scanner input;
 
-    public HumanPlayer(String name) {
+    public HumanPlayer(String name, Scanner input) {
         super(name);
+        this.input = input;
     }
 
     // Returns the player's guess
     @Override
     public int makeGuess() {
         System.out.println("Enter your guess: ");
-        int guess = 0;
-        boolean validInput = false;
-        while (!validInput) {
+        int guess;
+        while (true) {
             guess = input.nextInt();
-            if (guess >= 1 && guess <= 100) {
-//                System.out.println("Nice selection: " + guess);
-                validInput = true;
-            } else {
-                System.out.println("You are out of range. Please enter a number between 1 and 100.");
-            }
+            if (guess >= 1 && guess <= 100) break;
+            else System.out.println("You are out of range. Please enter a number between 1 and 100.");
         }
         guesses.add(guess);
         return guess;
